0199d19196cd03b062a119c40739ae1c3b7cf5d6
H-Maksym <105509906+H-Maksym@users.noreply.github.com>
2022-09-22 13:36:46 +0000

    Deploying to gh-pages from @ H-Maksym/goit-react-hw-05-movies@a51d02f9080d208adce0b9885618ae1927fb515e 🚀

---
+++ static/js/Reviews.887c589e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[753],{3595:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});r(2791);var n=r(184);function u(e){return(0,n.jsx)("div",{})}}}]);
+//# sourceMappingURL=Reviews.887c589e.chunk.js.map
